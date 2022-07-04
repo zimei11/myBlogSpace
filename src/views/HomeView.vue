f2673151cb46a2c97fde1aec9dea9051ed1e5864
zimei li <2928577064@qq.com>
2022-07-04 09:04:40 +0800

    导航栏与首页

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="card">
+        <div class="card-body">
+          首页
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style>
+.card{
+  margin-top: 20px;
+}
+</style>
